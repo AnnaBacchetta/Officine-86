53b62507564e151d4c8cdf162e6b69c9583b59a0
AnnaBacchetta <anna1.bacchetta@mail.polimi.it>
2025-05-30 13:03:40 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,161 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-kitchen",
+      "name": "Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-open-space",
+      "name": "Open space",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.21851247298280185,
+          "pitch": 0.05846320942749372,
+          "rotation": 0,
+          "target": "0-kitchen"
+        },
+        {
+          "yaw": 0.3551156978459531,
+          "pitch": 0.42088601251052715,
+          "rotation": 0,
+          "target": "2-living"
+        },
+        {
+          "yaw": -0.9317125298919375,
+          "pitch": -0.00927113843707339,
+          "rotation": 0,
+          "target": "3-corridor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-living",
+      "name": "Living",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-corridor",
+      "name": "Corridor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2797070052102946,
+          "pitch": 0.08379590810485738,
+          "rotation": 0,
+          "target": "1-open-space"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
